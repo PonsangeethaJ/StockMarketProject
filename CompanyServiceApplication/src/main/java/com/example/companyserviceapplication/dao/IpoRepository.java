5c1fb19f8c027ffa8b71d9e93d0bc05b8c051d1a
PonsangeethaJ <95565855+PonsangeethaJ@users.noreply.github.com>
2021-12-10 14:54:18 +0530

    Add files via upload

---
+++ CompanyServiceApplication/src/main/java/com/example/companyserviceapplication/dao/IpoRepository.java
@@ -0,0 +1,16 @@
+package com.example.companyserviceapplication.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.companyserviceapplication.model.Ipo;
+
+@Repository
+public interface IpoRepository extends MongoRepository<Ipo, String>
+{
+	public Optional<Ipo> findById(String id);
+	public void deleteById(String id);
+}
